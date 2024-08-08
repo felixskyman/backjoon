6f2d130468c1aa2920d8f07b18ae85773253a2c8
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-08-08 15:58:12 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("The 1-3-sum is %d", 91 + a + 3 * b + c);
+    return 0;
+}
