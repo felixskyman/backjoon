1307117cfffb4c7eee52c28de3562eca5d68bb34
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-08-09 12:11:26 +0900

    [Silver IV] Title: 상근이의 여행, Time: 28 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int T, N, M;
+    int a, b;
+    int i;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d %d", &N, &M);
+        for (i = 0; i < M; i++)
+        {
+            scanf("%d %d", &a, &b);
+        }
+        printf("%d\n", N - 1);
+    }
+}
