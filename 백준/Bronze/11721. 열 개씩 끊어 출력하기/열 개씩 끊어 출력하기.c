c410ef45fd2bb8720c5c89dcad292fb7a3ca6105
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-08-27 19:38:04 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100] = { 0, };
+
+    scanf("%s", str);
+
+    printf("%c", str[0]);
+    for (int i = 1; i < strlen(str); i++) {
+        if (i % 10 == 0) printf("\n");
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
