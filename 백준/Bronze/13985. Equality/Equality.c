031ce0ed5b73c7f0d5961bf8ddd22af1e860560d
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-08-27 19:47:44 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d + %d = %d", &a, &b, &c);
+	if (a+b==c)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO");
+	}
+			
+    return 0;
+}
