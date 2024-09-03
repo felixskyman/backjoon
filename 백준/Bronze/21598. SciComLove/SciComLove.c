2fc65744e9cf83bbbd82c5b53b3245d666cbba77
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-09-03 11:53:47 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++)
+        printf("SciComLove\n");
+    return (0);
+}
