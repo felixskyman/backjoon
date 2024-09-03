c930b25fcf5a44bc2a74ba52473adc043e7db886
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-09-03 11:52:56 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main() {
+    int K,D,A;
+    scanf("%d/%d/%d", &K, &D, &A);
+    if ((K+A)<D || D==0) {
+        printf("hasu");
+    }
+    else {
+        printf("gosu");
+    }
+    return 0;
+}
