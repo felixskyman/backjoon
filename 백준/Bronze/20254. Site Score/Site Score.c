2a8310678ba3e949fdd6e36601d153f9a8e3e4bc
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-09-03 11:50:05 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+int main() {
+    int ur,tr,uo,to;
+    scanf("%d %d %d %d", &ur, &tr, &uo, &to);
+    printf("%d", 56*ur+24*tr+14*uo+6*to);
+    return 0;
+}
