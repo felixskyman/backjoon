c5959f89c291b19fd3df0061cda7805c45965000
sheungho <142906283+felixskyman@users.noreply.github.com>
2024-12-30 14:50:16 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int size = n / k; 
+        int[] answer = new int[size];
+        
+        for (int i = 1; i <= size; i++) {
+            answer[i - 1] = k * i; 
+        }
+        
+        return answer;
+    }
+}
